acada5aa5c1c5ed00bbafea634ab612ada82d6d5
ahsan_m <ahsan_m@worksap.co.jp>
2019-03-27 16:48:16 +0800

    revert onload modify js

--- www/ASWebAuthSession.js
+++ www/ASWebAuthSession.js
@@ -1,19 +1,16 @@
-cordova.define("cordova-plugin-ios-aswebauthenticationsession", function(require, exports, module) {
+var PLUGIN_NAME = 'ASWebAuthSession';
+function ASWebAuthSession() {}
 
-	var PLUGIN_NAME = 'ASWebAuthSession';
-	function ASWebAuthSession() {}
+ASWebAuthSession.prototype.start = function(redirectScheme, requestURL, callback, errorCallback) {
+  exec(callback, errorCallback, PLUGIN_NAME, 'start', [redirectScheme, requestURL]);
+}
 
-	ASWebAuthSession.prototype.start = function(redirectScheme, requestURL, callback, errorCallback) {
-    exec(callback, errorCallback, PLUGIN_NAME, 'start', [redirectScheme, requestURL]);
+ASWebAuthSession.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
   }
 
-  ASWebAuthSession.install = function () {
-	  if (!window.plugins) {
-	    window.plugins = {};
-	  }
-
-	  window.plugins.ASWebAuthSession = new ASWebAuthSession();
-	  return window.plugins.ASWebAuthSession;
-	};
-	cordova.addConstructor(ASWebAuthSession.install);
-});
+  window.plugins.ASWebAuthSession = new ASWebAuthSession();
+  return window.plugins.ASWebAuthSession;
+};
+cordova.addConstructor(ASWebAuthSession.install);
